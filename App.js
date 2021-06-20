0f43853f2a85dd1a2e9531093c5fa5d5e78deb6a
j9311 <avilapost93@gmail.com>
2021-06-20 13:34:16 -0700

    legato add

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    
+
+    <ImageBackground source={require('./assets/background.jpg')} style={styles.container}>
+      <View style={styles.overlay}>
       <StatusBar style="auto" />
-    </View>
+      </View>
+
+
+      <Text style={styles.legato}>Legato</Text>
+      
+      <Text style={styles.text}>Create</Text>
+        <br/>
+      <Text style={styles.text}>Connect</Text>
+        <br/>
+      <Text style={styles.text}>Collaborate</Text>
+
+
+      <TouchableOpacity onPress={() => console.log('Thanks for visiting!')} style={styles.button} accessibilityLabel="Use this to sign in to or sign up for Legato!">
+        <Text style={styles.buttonText}>Sign in / Join</Text>
+      </TouchableOpacity>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'white',
   },
+  image: {
+    flex: 1, 
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  }, 
+  legato: {
+    color: 'white', 
+    fontSize: '3.75em',
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    fontFamily: 'Georgia',
+    fontStyle: 'italic',
+    marginBottom: '2em',
+    zIndex: 0
+  },
+  text: {
+    display: 'flex',
+    color: 'white',
+    fontSize: '2.5em',
+    alignSelf: 'baseline',
+    fontWeight: 'bold',
+    marginLeft: '1em',
+    fontFamily: 'Arial',
+    zIndex: 0,
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'black',
+    opacity: 0.6
+  },
+  button: {
+    backgroundColor: "black",
+    padding: 5,
+    borderRadius: 8,
+    width: '18em',
+    textAlign: 'center',
+  },
+  buttonText: {
+    fontSize: '1em',
+    color: '#fff',
+  }, 
+  
 });
