ff62fc1f6b6cf51bd3112d04d36f841c2eacbc61
j9311 <avilapost93@gmail.com>
2021-06-20 18:24:01 -0700

    font fix initial

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { ImageBackground, StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
+
 export default function App() {
   return (
     
